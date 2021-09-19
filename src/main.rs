0a3d7055afc900c0d3f05fb529908f2c06fa7ffd
gbaranski <root@gbaranski.com>
2021-09-19 13:03:26 +0200

    feat: bigger window size

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,16 @@ use misc::Side;
 use misc::Velocity;
 use world::World;
 
-#[macroquad::main("bila")]
+fn window_configuration() -> macroquad::window::Conf {
+    macroquad::window::Conf {
+        window_title: "Bila".to_string(),
+        window_width: 1280,
+        window_height: 720,
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(window_configuration)]
 async fn main() {
     let font = {
         let font = macroquad::text::load_ttf_font_from_bytes(fonts::SOURCE_CODE_PRO_LIGHT)
